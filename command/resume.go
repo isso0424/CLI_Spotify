98a7e3dd53c047d63b7d686c289bf5bff408db47
isso0424 <isso04240129@gmail.com>
2020-07-11 04:48:11 +0900

    [Add]Resumeを実装した

--- command/resume.go
+++ command/resume.go
@@ -1,1 +1,27 @@
 package command
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"golang.org/x/oauth2"
+)
+
+func Resume(token *oauth2.Token) {
+  values := url.Values{}
+  values.Add("context_uri", "spotify:playlist:4Cu0w3ZsPYiPQTeElf2Tls")
+  request, err := http.NewRequest("PUT", "https://api.spotify.com/v1/me/player/play", nil)
+  if err != nil {
+    fmt.Printf(err.Error())
+  }
+
+  request.Header.Set("Authorization", "Bearer " + token.AccessToken)
+  client := &http.Client{}
+  response, err := client.Do(request)
+  if err != nil {
+    fmt.Printf(err.Error())
+    return
+  }
+  defer response.Body.Close()
+}
