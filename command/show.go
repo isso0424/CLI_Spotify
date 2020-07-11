e478a2eeb5a46bfa045f4d991585d0a660d9cd93
isso0424 <isso04240129@gmail.com>
2020-07-12 07:38:30 +0900

    [Add]playlist一覧を参照できるようになった

---
+++ command/show.go
@@ -0,0 +1,29 @@
+package command
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func Show() {
+  if !existFile("playlist.json") {
+    fmt.Println("first, you have to save playlist")
+    return
+  }
+
+  file, err := ioutil.ReadFile("playlist.json")
+  if err != nil {
+    log.Fatalln("could not read playlist.json")
+    return
+  }
+
+  var playlistList []playlist
+
+  json.Unmarshal(file, &playlistList)
+
+  for index, target := range playlistList {
+    fmt.Printf("%d\n------------------------------------------------\nname: %s\nuri: %s\n\n", index, target.Name, target.Uri)
+  }
+}
--- main.go
+++ main.go
@@ -50,6 +50,8 @@ func mainLoop(token string) {
       command.Save()
     case "load":
       command.Load(token)
+    case "show":
+      command.Show()
     case "refresh":
       token, _ = auth.GetToken(true)
   }
