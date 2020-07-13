5da646f25ca868a601e752eca585e684b283465e
isso0424 <isso04240129@gmail.com>
2020-07-13 14:40:02 +0900

    [Add]Getリクエストが失敗したときのエラーを定義した

---
+++ selfMadeTypes/failedGetError.go
@@ -0,0 +1,11 @@
+package selfMadeTypes
+
+import "fmt"
+
+type FailedGetError struct {
+  Target string
+}
+
+func(ptr *FailedGetError) Error() string {
+  return fmt.Sprintf("Failed to get %s", ptr.Target)
+}
