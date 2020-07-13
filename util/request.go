daed5c5a6bf34ad89b7fc55d7c4e23e30a38439c
isso0424 <isso04240129@gmail.com>
2020-07-13 13:33:29 +0900

    [Move]createRequestをutil配下に移動

---
+++ util/request.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"io"
+	"isso0424/spotify_CLI/auth"
+	"net/http"
+)
+
+func createRequest(token string, method string, url string, body io.Reader) (response *http.Response, newToken string, err error) {
+	newToken = token
+	request, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return
+	}
+
+	request.Header.Set("Authorization", "Bearer "+token)
+	client := &http.Client{}
+	response, err = client.Do(request)
+	if err != nil {
+		return
+	}
+
+	if response.StatusCode == 401 {
+		newTokenPtr, Err := auth.GetToken()
+		if err != nil {
+			err = Err
+			return
+		}
+
+		newToken = *newTokenPtr
+	}
+
+	return
+}
