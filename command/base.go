e62f166b7c0856c2f1e4010aed5aff49f28d1d54
isso0424 <isso04240129@gmail.com>
2020-07-13 13:32:47 +0900

    [Add]commandの分岐をcommand/base.goに移行

--- command/base.go
+++ command/base.go
@@ -1,34 +1,55 @@
 package command
 
 import (
-	"io"
-	"isso0424/spotify_CLI/auth"
-	"net/http"
+	"fmt"
+	"isso0424/spotify_CLI/util"
 )
 
-func createRequest(token string, method string, url string, body io.Reader) (response *http.Response, newToken string, err error) {
-	newToken = token
-	request, err := http.NewRequest(method, url, body)
-	if err != nil {
-		return
+
+func MainLoop(token string) {
+	fmt.Println("if you wanna exit, you must type 'exit'")
+	GetPlayStatus(token)
+	for {
+		var commandName string
+		util.Input("Command", &commandName)
+
+    if commandName == "exit" {
+      break
+    }
+    token = command(token, commandName)
+	}
+}
+
+func command(token string, commandName string) (newToken string) {
+  newToken = token
+	switch commandName {
+	case "pause":
+		newToken = Pause(newToken)
+	case "resume":
+		newToken = Resume(newToken)
+	case "status":
+		_, newToken = GetPlayStatus(newToken)
+	case "play":
+		newToken = PlayFromURL(newToken)
+	case "save":
+		Save()
+	case "load":
+		Load(newToken)
+	case "show":
+		Show()
+	case "refresh":
+		newToken = Refresh()
+	case "random":
+		newToken = Random(newToken)
+	case "next":
+		newToken = Next(newToken)
+	case "prev":
+		newToken = Prev(newToken)
+	case "repeat":
+		newToken = Repeat(newToken)
+	case "shuffle":
+		newToken = Shuffle(newToken)
 	}
 
-	request.Header.Set("Authorization", "Bearer "+token)
-	client := &http.Client{}
-	response, err = client.Do(request)
-	if err != nil {
-		return
-	}
-
-	if response.StatusCode == 401 {
-		newTokenPtr, Err := auth.GetToken()
-		if err != nil {
-			err = Err
-			return
-		}
-
-		newToken = *newTokenPtr
-	}
-
-	return
+  return
 }
--- main.go
+++ main.go
@@ -1,62 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-
 	"isso0424/spotify_CLI/auth"
 	"isso0424/spotify_CLI/command"
-	"isso0424/spotify_CLI/util"
 )
 
 func main() {
 	token, err := auth.GetToken()
 
-	fmt.Println(strings.Split("https://open.spotify.com/playlist/2EKvIPR0K0rvpNnyhKdsd6?si=nUEPESAtSiywv8GkWAHYjg", "/")[3])
-
 	if err != nil {
 		panic(err)
 	}
 
-	mainLoop(*token)
+	command.MainLoop(*token)
 }
 
-func mainLoop(token string) {
-	fmt.Println("if you wanna exit, you must type 'exit'")
-	command.GetPlayStatus(token)
-	for {
-		var commandKind string
-		util.Input("Command", &commandKind)
-
-		switch commandKind {
-		case "exit":
-			return
-		case "pause":
-			token = command.Pause(token)
-		case "resume":
-			token = command.Resume(token)
-		case "status":
-			_, token = command.GetPlayStatus(token)
-		case "play":
-			token = command.PlayFromURL(token)
-		case "save":
-			command.Save()
-		case "load":
-			command.Load(token)
-		case "show":
-			command.Show()
-		case "refresh":
-			token = command.Refresh()
-		case "random":
-			token = command.Random(token)
-		case "next":
-			token = command.Next(token)
-		case "prev":
-			token = command.Prev(token)
-		case "repeat":
-			token = command.Repeat(token)
-		case "shuffle":
-			token = command.Shuffle(token)
-		}
-	}
-}
