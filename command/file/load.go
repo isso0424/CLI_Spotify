49b2086d34cf3dbc85f3e4266ff76a69f9d7eb0b
isso0424 <isso04240129@gmail.com>
2020-07-24 04:05:05 +0900

    [Update]ファイルロードをテストできる状態にした

--- command/file/load.go
+++ command/file/load.go
@@ -7,12 +7,27 @@ import (
 	"os"
 )
 
+var (
+  fileExist func(string) (os.FileInfo, error)
+  readFile func(string) ([]byte, error)
+)
+
+func init() {
+  fileExist = func (fileName string) (os.FileInfo, error) {
+    return os.Stat(fileName)
+  }
+
+  readFile = func (fileName string) ([]byte, error) {
+    return ioutil.ReadFile(fileName)
+  }
+}
+
 func LoadPlayList() (playlistList []selfMadeTypes.PlayList, err error) {
-	if _, err = os.Stat("playlist.json"); err != nil {
+	if _, err = fileExist("playlist.json"); err != nil {
 		return
 	}
 
-	file, err := ioutil.ReadFile("playlist.json")
+	file, err := readFile("playlist.json")
 	if err != nil {
 		return
 	}
