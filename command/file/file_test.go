df50549e264ef92057c9353a728cac5936cfb5a2
isso0424 <isso04240129@gmail.com>
2020-07-24 05:10:01 +0900

    [Add]ファイルをロードするテストを書いた

---
+++ command/file/file_test.go
@@ -0,0 +1,69 @@
+package file
+
+import (
+	"encoding/json"
+	"isso0424/spotify_CLI/selfMadeTypes"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadPlayList(t *testing.T) {
+  reset := SetLoadPlayList(
+    func(fileName string) (os.FileInfo, error) {
+      fileInfo := files{}
+      return fileInfo, nil
+    },
+    func(fileName string) ([]byte, error) {
+      playlistList := []selfMadeTypes.PlayList{
+        {
+          Name: "PlayList",
+          Uri: "URI",
+        },
+      }
+	    return json.Marshal(playlistList)
+    },
+  )
+  defer reset()
+
+  successResult, _ := LoadPlayList()
+  assert.Equal(
+    t,
+    []selfMadeTypes.PlayList{
+      {
+        Name: "PlayList",
+        Uri: "URI",
+      },
+    },
+    successResult,
+  )
+}
+
+type files struct {
+}
+
+func(f files) Name() string {
+  return "file"
+}
+
+func(f files) Size() int64 {
+  return 1
+}
+
+func(f files) Mode() os.FileMode{
+  return 0600
+}
+
+func(f files) ModTime() time.Time {
+  return time.Time{}
+}
+
+func(f files) IsDir() bool {
+  return false
+}
+
+func(f files) Sys() interface{} {
+  return ""
+}
