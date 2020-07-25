b63da9f7a1453198a6ea33f0435e7539d43df369
isso0424 <isso04240129@gmail.com>
2020-07-26 06:26:21 +0900

    [Add]検索結果の出力処理を実装した

--- selfMadeTypes/search.go
+++ selfMadeTypes/search.go
@@ -1,5 +1,7 @@
 package selfMadeTypes
 
+import "fmt"
+
 type SearchResponse struct {
   Album    searchAlbum    `json:"albums"`
   Artists  searchArtists  `json:"artists"`
@@ -7,6 +9,43 @@ type SearchResponse struct {
   Playlist searchPlayList `json:"playlists"`
   Show     searchShow     `json:"shows"`
   Episode  searchEpisode  `json:"episodes"`
+}
+
+func (response SearchResponse) ParseAndPrint(kinds []string) {
+  for _, kind := range(kinds) {
+    switch (kind) {
+    case "album":
+      fmt.Println("-----Albums-----")
+      for _, album := range(response.Album.Item) {
+        fmt.Printf("Name: %s\nURI: %s\n---------------\n", album.Name, album.Uri)
+      }
+    case "artist":
+      fmt.Println("-----Artists-----")
+      for _, artist := range(response.Artists.Item) {
+        fmt.Printf("Name: %s\nURI: %s\n---------------\n", artist.Name, artist.Uri)
+      }
+    case "track":
+      fmt.Println("-----Tracks-----")
+      for _, track := range(response.Track.Item) {
+        fmt.Printf("Name: %s\nURI: %s\n---------------\n", track.Name, track.Uri)
+      }
+    case "playlist":
+      fmt.Println("-----Playlists-----")
+      for _, playlist := range(response.Playlist.Item) {
+        fmt.Printf("Name: %s\nURI: %s\n---------------\n", playlist.Name, playlist.Uri)
+      }
+    case "show":
+      fmt.Println("-----Shows-----")
+      for _, show := range(response.Show.Item) {
+        fmt.Printf("Name: %s\nURI: %s\n---------------\n", show.Name, show.Uri)
+      }
+    case "episode":
+      fmt.Println("-----Episodes-----")
+      for _, episode := range(response.Episode.Item) {
+        fmt.Printf("Name: %s\nURI: %s\n---------------\n", episode.Name, episode.Uri)
+      }
+    }
+  }
 }
 
 type searchAlbum struct {
