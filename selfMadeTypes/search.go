c896403f3c13655070e17970ade0d9791ee797dc
isso0424 <isso04240129@gmail.com>
2020-07-26 05:51:30 +0900

    [Update]Jsonの型定義のガバを修正

--- selfMadeTypes/search.go
+++ selfMadeTypes/search.go
@@ -1,12 +1,36 @@
 package selfMadeTypes
 
 type SearchResponse struct {
-  Album    []album    `json:"albums"`
-  Artists  []Artists  `json:"artists"`
-  Track    []track    `json:"tracks"`
-  Playlist []PlayList `json:"playlists"`
-  Show     []show     `json:"shows"`
-  Episode  []episode  `json:"episodes"`
+  Album    searchAlbum    `json:"albums"`
+  Artists  searchArtists  `json:"artists"`
+  Track    searchAlbum    `json:"tracks"`
+  Playlist searchPlayList `json:"playlists"`
+  Show     searchShow     `json:"shows"`
+  Episode  searchEpisode  `json:"episodes"`
+}
+
+type searchAlbum struct {
+  Item []album `json:"items"`
+}
+
+type searchArtists struct {
+  Item []Artists `json:"items"`
+}
+
+type searchPlayList struct {
+  Item []PlayList `json:"items"`
+}
+
+type searchShow struct {
+  Item []show `json:"items"`
+}
+
+type searchEpisode struct {
+  Item []episode `json:"items"`
+}
+
+type searchTrack struct {
+  Item []track `json:"items"`
 }
 
 type track struct {
