36658aac59d6460315d8d66e22e9332c15e96db9
isso0424 <isso04240129@gmail.com>
2020-07-27 02:51:50 +0900

    [fix]gofmtかけた

--- util/inputter.go
+++ util/inputter.go
@@ -7,10 +7,10 @@ import (
 )
 
 func Input(message string, hint string, getValiable *string) {
-  scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Printf("%s\n%s|>>>", message, hint)
-  if scanner.Scan() {
-    *getValiable = scanner.Text()
-  }
+	if scanner.Scan() {
+		*getValiable = scanner.Text()
+	}
 	fmt.Println()
 }
