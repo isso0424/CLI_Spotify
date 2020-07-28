591508366bd69156453506b6e0dccd9790cc9e0a
isso0424 <isso04240129@gmail.com>
2020-07-29 02:32:26 +0900

    [Add]必要な型を定義した

---
+++ selfmadetypes/recent.go
@@ -0,0 +1,22 @@
+package selfmadetypes
+
+// RecentPlayedTracks is response of Get recently played track.
+type RecentPlayedTracks struct{
+	Items []trackSimplified `json:"items"`
+}
+
+type trackSimplified struct{
+	Artists []Artists `json:"artists"`
+	AvailableMarkets []string `json:"available_markets"`
+	DiscNumber int `json:"disc_number"`
+	Duration int `json:"duration_ms"`
+	ExternalUrls externalUrls `json:"external_urls"`
+	Href string `json:"href"`
+	ID string `json:"id"`
+	IsPlayable bool `json:"is_playable"`
+	Name string `json:"name"`
+	PreviewURL string `json:"preview_url"`
+	TrackNumber int `json:"track_number"`
+	Type string `json:"type"`
+	URI string `json:"uri"`
+}
