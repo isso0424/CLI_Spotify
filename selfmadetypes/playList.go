85823de877921de59a75039e0eabeaec95dc1062
isso0424 <isso04240129@gmail.com>
2020-07-29 19:17:16 +0900

    [Add]型定義を追加

--- selfmadetypes/playList.go
+++ selfmadetypes/playList.go
@@ -10,4 +10,17 @@ type PlayList struct {
 type PlayListFromRequest struct {
 	Name  string `json:"name"`
 	Owner User   `json:"owner"`
+	Href  string `json:"href"`
+	Followers followers `json:"followers"`
+	Tracks playlistPaging `json:"tracks"`
+}
+
+type followers struct {
+	Href string `json:"href"`
+	Total int `json:"Total"`
+}
+
+type playlistPaging struct {
+	Href string `json:"href"`
+	Total int `json:"total"`
 }
