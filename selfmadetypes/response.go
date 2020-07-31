b2dd68e00689fe7ae11de8175cdce94b6c8ae020
isso0424 <isso04240129@gmail.com>
2020-07-31 10:55:20 +0900

    [Add]Defined Response interface

---
+++ selfmadetypes/response.go
@@ -0,0 +1,43 @@
+package selfmadetypes
+
+import (
+	"bytes"
+	"net/http"
+)
+
+// Response is interafce for HTTP request response.
+type Response interface {
+	GetBody() []byte
+	GetStatusCode() int
+}
+
+// HttpResponse is struct for HTTP request response.
+type HttpResponse struct {
+	Body []byte
+	StatusCode int
+}
+
+func (response HttpResponse) GetBody() []byte {
+	return response.Body
+}
+
+func (response HttpResponse) GetStatusCode() int {
+	return response.StatusCode
+}
+
+func (tmp HttpResponse) New(response *http.Response) (HttpResponse, error) {
+	statusCode := response.StatusCode
+
+	responseArray := make([]byte, 32768)
+	_, err := response.Body.Read(responseArray)
+	if err != nil {
+		return HttpResponse{}, err
+	}
+
+	responseArray = bytes.Trim(responseArray, "\x00")
+
+	return HttpResponse{
+		Body: responseArray,
+		StatusCode: statusCode,
+	}, nil
+}
