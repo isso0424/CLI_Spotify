34d08f12d56f9ff2f8060a97d9b745ba1c4ff8bb
isso0424 <isso04240129@gmail.com>
2020-08-08 15:48:15 +0900

    [Fix]fix splitter length for multi byte charactors.

--- util/outputter.go
+++ util/outputter.go
@@ -22,10 +22,22 @@ func Output(message selfmadetypes.OutputMessage) {
 func getMaxLengthRow(message selfmadetypes.OutputMessage) (maxLength int) {
 	for _, lump := range message.Message {
 		for _, row := range lump {
-			if len(row) > maxLength {
-				maxLength = len(row)
+			if stringLengthCounter(row) > maxLength {
+				maxLength = stringLengthCounter(row)
 			}
 		}
+	}
+
+	return
+}
+
+func stringLengthCounter(str string) (length int) {
+	runes := []rune(str)
+	for _, charactor := range runes {
+		if charactor > 255 {
+			length++
+		}
+		length++
 	}
 
 	return
